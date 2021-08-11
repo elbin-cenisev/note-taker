7a67fddb8ecbea7d64a2192d47649b13ce14c235
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-10 22:43:40 -0400

    Now listens to port

--- server.js
+++ server.js
@@ -12,3 +12,7 @@ app.use(express.static('public'));
 app.get('/', (req, res) => 
     res.sendFile(path.join(__dirname, '/public/index.html'))
 );
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
